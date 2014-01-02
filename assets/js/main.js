1440f543eebf0ce119c647cfd99186be4e9ca68f
Simakov Konstantin <gigabytetheone@gmail.com>
2014-01-02 23:48:29 +0600

    Portfolio section beginning

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$ = jQuery;
+
+$(function () {
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        var pos = target.offset().top;
+        if ($('#mainHeader').outerHeight() == 0) {
+          pos -= $('.navbar-fixed-top').outerHeight();
+        }
+        $('html,body').animate({
+          scrollTop: pos
+        }, 600);
+        // return false;
+      }
+    }
+  });
+
+});
+
